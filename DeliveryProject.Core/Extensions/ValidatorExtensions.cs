3d90c485378964c2f552eb7d39cda3afc4910a9e
M3rcy <m3rcy139@gmail.com>
2025-03-13 15:19:06 +0300

    Combined EntityExtensions and ValidatorExtensions

--- DeliveryProject.Core/Extensions/ValidatorExtensions.cs
+++ DeliveryProject.Core/Extensions/ValidatorExtensions.cs
@@ -19,5 +19,23 @@ namespace DeliveryProject.Core.Extensions
                 throw new BussinessArgumentException(errorMessage);
             }
         }
+
+        public static void ValidateEntity<T>(this T entity, string errorMessage, string errorCode)
+        where T : class
+        {
+            if (entity == null)
+            {
+                throw new BussinessArgumentException(errorMessage, errorCode);
+            }
+        }
+
+        public static void ValidateEntities<T>(this IEnumerable<T?> entities, string errorMessage, string errorCode)
+        where T : class
+        {
+            if (!entities.Any() || entities.Any(e => e == null))
+            {
+                throw new BussinessArgumentException(errorMessage, errorCode);
+            }
+        }
     }
 }
