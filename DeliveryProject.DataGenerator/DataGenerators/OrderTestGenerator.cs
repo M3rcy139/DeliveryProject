f466e78c77a0662c59afcb855a1c117156a60663
M3rcy <m3rcy139@gmail.com>
2025-04-02 23:34:39 +0300

    Refactor OrderTestGenerator

--- DeliveryProject.DataGenerator/DataGenerators/OrderTestGenerator.cs
+++ DeliveryProject.DataGenerator/DataGenerators/OrderTestGenerator.cs
@@ -2,6 +2,7 @@
 using DeliveryProject.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using DeliveryProject.Core.Enums;
+using DeliveryProject.DataGenerator.Helpers;
 
 namespace DeliveryProject.DataGenerator.DataGenerators
 {
@@ -11,6 +12,7 @@ namespace DeliveryProject.DataGenerator.DataGenerators
         {
             var random = new Random();
             var orders = new List<OrderEntity>();
+            var invoices = new List<InvoiceEntity>();
 
             var customers = await context.Persons
                 .OfType<CustomerEntity>()
@@ -39,24 +41,18 @@ namespace DeliveryProject.DataGenerator.DataGenerators
                     OrderPersons = new List<OrderPersonEntity>
                 {
                     new OrderPersonEntity { PersonId = customer.Id },
-                    new OrderPersonEntity { PersonId = deliveryPerson.Id },
                 },
                     OrderProducts = orderProducts,
-                    // Invoice = new InvoiceEntity
-                    // {
-                    //     Id = Guid.NewGuid(),
-                    //     OrderId = Guid.NewGuid(),
-                    //     DeliveryPersonId = deliveryPerson.Id,
-                    //     Amount = amount,
-                    //     DeliveryTime = new DateTime(2027, 10, 21, random.Next(8, 11), random.Next(0, 60), 0, DateTimeKind.Utc),
-                    //     IsExecuted = false
-                    // }
                 };
 
                 orders.Add(order);
+
+                var invoice = InvoiceTestHelper.CreateInvoice(order.Id, deliveryPerson.Id);
+                invoices.Add(invoice);
             }
 
             await context.Orders.AddRangeAsync(orders);
+            await context.Invoices.AddRangeAsync(invoices);
             await context.SaveChangesAsync();
         }
     }
