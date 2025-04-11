443aef140a42c49598a27d4e36c716319d5af962
M3rcy <m3rcy139@gmail.com>
2025-04-11 21:47:33 +0300

    Added some migrations to github

---
+++ DeliveryProject.Migrations/Migrations/20250402114251_AddAmountToOrder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryProject.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmountToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Invoices");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Orders",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Invoices",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
