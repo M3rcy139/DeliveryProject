becfbb2feec10a8ec3a6908073d55e6361ffd628
M3rcy <m3rcy139@gmail.com>
2025-04-21 15:34:34 +0300

    Replaced MongoDB with ElasticSearch

--- DeliveryProject/ServiceCollection/LoggingConfiguration.cs
+++ DeliveryProject/ServiceCollection/LoggingConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using Serilog.Sinks.Elasticsearch;
+using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
 using RollingInterval = Serilog.RollingInterval;
@@ -9,7 +10,7 @@ namespace DeliveryProject.ServiceCollection
     {
         public static void ConfigureLogging(this IHostBuilder hostBuilder, IConfiguration configuration)
         {
-            var mongoConnection = configuration.GetConnectionString("MongoDbLogs");
+            var elasticUri = configuration["ElasticConfiguration:Uri"];
             
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(configuration)
@@ -21,10 +22,14 @@ namespace DeliveryProject.ServiceCollection
                     $"logs/log-{DateTime.UtcNow:yyyy-MM-dd}.json",
                     rollingInterval: RollingInterval.Day,
                     retainedFileCountLimit: 7)
-                .WriteTo.MongoDBBson(
-                    mongoConnection!,
-                    collectionName: "applogs")
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri!))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"deliveryproject-logs-{DateTime.UtcNow:yyyy-MM}",
+                    MinimumLogEventLevel = LogEventLevel.Information,
+                    NumberOfShards = 2,
+                    NumberOfReplicas = 1
+                })
                 .CreateLogger();
 
             hostBuilder.UseSerilog();
