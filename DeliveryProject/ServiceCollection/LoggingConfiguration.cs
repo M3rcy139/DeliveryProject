5c8d398d669f0b7d9258ba524032f14c47908981
M3rcy <m3rcy139@gmail.com>
2025-04-23 00:32:32 +0300

    Moved logging variables to appsettings

--- DeliveryProject/ServiceCollection/LoggingConfiguration.cs
+++ DeliveryProject/ServiceCollection/LoggingConfiguration.cs
@@ -11,28 +11,27 @@ namespace DeliveryProject.ServiceCollection
     {
         public static void ConfigureLogging(this IHostBuilder hostBuilder, IConfiguration configuration)
         {
-            var elasticUri = configuration["ElasticConfiguration:Uri"];
             var logSettings = configuration.GetSection("Logging").Get<LoggingSettings>();
             
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Is(logSettings!.DefaultLogLevel)
                 .MinimumLevel.Override("Microsoft.AspNetCore", logSettings.AspNetCoreLogLevel)
                 .Enrich.FromLogContext()
-                .Enrich.WithProperty("Application", "DeliveryProject")
+                .Enrich.WithProperty("Application", logSettings.ApplicationName)
                 .WriteTo.Console(restrictedToMinimumLevel: logSettings.ConsoleLogLevel)
                 .WriteTo.File(
                     new RenderedCompactJsonFormatter(),
                     path: logSettings.FilePath,
                     rollingInterval: RollingInterval.Day,
                     restrictedToMinimumLevel: logSettings.FileLogLevel,
-                    retainedFileCountLimit: 7)
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri!))
+                    retainedFileCountLimit: logSettings.RetainedFileCountLimit)
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]!))
                 {
                     AutoRegisterTemplate = true,
-                    IndexFormat = $"deliveryproject-logs-{DateTime.UtcNow:yyyy-MM}",
+                    IndexFormat = logSettings.ElasticIndexFormat,
                     MinimumLogEventLevel = logSettings.ElasticSearchLogLevel,
-                    NumberOfShards = 2,
-                    NumberOfReplicas = 1
+                    NumberOfShards = logSettings.ElasticSearchNumberOfShards,
+                    NumberOfReplicas = logSettings.ElasticSearchNumberOfReplicas,
                 })
                 .CreateLogger();
 
